97c08d3afc06feb8bb683d8ac221c4ce58ab4788
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-12 19:04:50 -0300

    initial commit

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useState } from "react";
+
+const Trips = () => {
+    const [name, setName] = useState();
+  return (
+    <div>Trips</div>
+  )
+}
+
+export default Trips
---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Trips from './components/Trips'
+
+export const metadata = {
+    title: "About",
+};
+
+const Page = () => {
+  return (
+    <div>
+        <Trips />
+    </div>
+  )
+};
+
+export default Page;
