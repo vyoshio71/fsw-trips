e3b12db2f1c0bed25d20d66b1cfb1ea624c7ae0c
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-13 17:25:20 -0300

    feat: add recommended trips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { signIn } from "next-auth/react";
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div className="">
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
