f2e62377291985839ba3928e6cd1e17910fb09d0
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-13 16:06:19 -0300

    feat: add tags quick search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import { signIn } from "next-auth/react";
 import TripSearch from "./components/TripSearch";
+import QuickSearch from "./components/QuickSearch";
 
 export default function Home() {
   return (
     <div className="">
       <TripSearch />
+      <QuickSearch />
     </div>
   );
 }
