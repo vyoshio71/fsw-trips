798eb4c140caff968a7b14d53e895f965c7a76b7
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-14 16:54:18 -0300

    feat: add trip highlight component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
 import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
 
 const getTripDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -21,9 +22,9 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
   return (
     <div className="container mx-auto">
       <TripHeader trip={trip} />
-      {/* Reserva */}
       <TripReservation trip={trip} />
-      <TripDescription description={trip.description}/>
+      <TripDescription description={trip.description} />
+      <TripHighlights highlights={trip.highlights} />
     </div>
   );
 };
