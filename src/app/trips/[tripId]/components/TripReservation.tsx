ec2cc2b303b9ae680bfb4e39d53bcf3a46cbb0e9
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-14 16:00:16 -0300

    style: add border to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -11,32 +11,32 @@ interface TripReservationProps {
 
 function TripReservation({ trip }: TripReservationProps) {
   return (
-    <div>
-      <div className="flex flex-col px-5">
-        <div className="flex gap-4">
-          <DatePicker
-            placeholderText="Data de início"
-            onChange={() => {}}
-            className="w-full"
-          />
-          <DatePicker
-            placeholderText="Data final"
-            onChange={() => {}}
-            className="w-full"
-          />
-        </div>
-
-        <Input
-          placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
-          className="mt-4"
+    <div className="flex flex-col px-5">
+      <div className="flex gap-4">
+        <DatePicker
+          placeholderText="Data de início"
+          onChange={() => {}}
+          className="w-full"
         />
+        <DatePicker
+          placeholderText="Data final"
+          onChange={() => {}}
+          className="w-full"
+        />
+      </div>
 
-        <div className="flex justify-between mt-3">
-          <p className="font-medium text-sm text-primaryDarker">Total: </p>
-          <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
-        </div>
+      <Input
+        placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+        className="mt-4"
+      />
 
-        <Button className="mt-3">Reservar agora</Button>
+      <div className="flex justify-between mt-3">
+        <p className="font-medium text-sm text-primaryDarker">Total: </p>
+        <p className="font-medium text-sm text-primaryDarker">R$2500 </p>
+      </div>
+
+      <div className="pb-10 border-b border-grayLighter w-full">
+        <Button className="mt-3 w-full">Reservar agora</Button>
       </div>
     </div>
   );
