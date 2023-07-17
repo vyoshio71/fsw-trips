5d2fb9e09b1ba828c7f6158670ccca4fc4235091
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-17 11:19:20 -0300

    feat: ensure user is redirected on error in trip confirmation

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -32,10 +32,14 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         }),
       });
 
-      const { trip, totalPrice } = await response.json();
+      const res = await response.json();
 
-      setTrip(trip);
-      setTotalPrice(totalPrice);
+      if (res?.error) {
+        return router.push("/");
+      }
+
+      setTrip(res.trip);
+      setTotalPrice(res.totalPrice);
     };
 
     if (status === "unauthenticated") {
