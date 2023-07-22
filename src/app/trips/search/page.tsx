764099bfe1244e29d820d31092c0a9375666ad26
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-07-22 15:21:59 -0300

    style: adapte search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,14 +29,16 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">
         Viagens encontradas
       </h1>
-      <h2 className="text-grayPrimary font-medium mb-5">
-        {trips.length > 0 ? 'Listamos as melhores viagens para você!' : 'Não encontramos nenhum resultado para sua busca!'}
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
+        {trips.length > 0
+          ? "Listamos as melhores viagens para você!"
+          : "Não encontramos nenhum resultado para sua busca!"}
       </h2>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
         {trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}
       </div>
     </div>
